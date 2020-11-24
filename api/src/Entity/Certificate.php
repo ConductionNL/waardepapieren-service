d59d70be8e7c3c00c2f1c5d0f775444e2157022a
Ruben van der Linde <ruben@conduction.nl>
2020-11-24 13:04:07 +0100

    Updates

--- api/src/Entity/Certificate.php
+++ api/src/Entity/Certificate.php
@@ -14,24 +14,9 @@ use Symfony\Component\Validator\Constraints as Assert;
  *     normalizationContext={"groups"={"read"}, "enable_max_depth"=true},
  *     denormalizationContext={"groups"={"write"}, "enable_max_depth"=true}
  * )
- * @ORM\Entity(repositoryClass="App\Repository\CertificateRepository")
  */
 class Certificate
 {
-    /**
-     * @var UuidInterface The UUID identifier of this resource
-     *
-     * @example e2984465-190a-4562-829e-a8cca81aa35d
-     *
-     * @Assert\Uuid
-     * @Groups({"read"})
-     * @ORM\Id
-     * @ORM\Column(type="uuid", unique=true)
-     * @ORM\GeneratedValue(strategy="CUSTOM")
-     * @ORM\CustomIdGenerator(class="Ramsey\Uuid\Doctrine\UuidGenerator")
-     */
-    private $id;
-
     /**
      * @var string The person for this certificate, this can be a bsn or 'haal centraal' uri.
      *
