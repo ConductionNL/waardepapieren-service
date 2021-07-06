766c449632694e3508bc25da3eeabb6ae2f8440f
rjzondervan <rjzondervan@gmail.com>
2021-07-06 18:29:52 +0000

    Readme update (badges + description)

--- api/docker/php/docker-entrypoint.sh
+++ api/docker/php/docker-entrypoint.sh
@@ -68,6 +68,7 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 			echo "Creating OAS documentation"
 			bin/console api:openapi:export --output=/srv/api/public/schema/openapi.yaml --yaml --spec-version=3
 
+
 			# this should only be done in an build
 			echo "Updating Helm charts"
 			bin/console app:helm:update --location=/srv/api/helm --spec-version=3
@@ -75,6 +76,10 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 			# this should only be done in an build
 			echo "Updating publiccode charts"
 			bin/console app:publiccode:update --location=/srv/api/public/schema/ --spec-version=0.2
+
+
+			echo "Run unit tests"
+			./vendor/bin/phpunit --coverage-clover coverage.xml
 		fi
 	fi
 fi
